c0b94a73104c7dfdea87fd4572e85c42aa778708
crukky <hiroki.kinoshita@nislab.doshisha.ac.jp>
2018-05-11 14:26:16 +0900

    dnn-research

--- compare.py
+++ compare.py
@@ -4,5 +4,15 @@ sys.path.append(os.pardir)
 import matplotlib.pyplot as plt
 import numpy as np
 
-for i in range(1000)
-    
+dataA = open("generalization_test0.txt","r")
+dataB = open("generalization_test1.txt","r")
+linesA = dataA.readlines()
+linesB = dataB.readlines()
+
+#正解数
+count = 0
+for i in range(10000):
+    if linesA[i] == linesB[i]:
+        count += 1
+
+print(count)
