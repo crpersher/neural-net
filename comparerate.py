91191d382328b615b4dd10d4f58abdc6fa21cbd4
crukky <hiroki.kinoshita@nislab.doshisha.ac.jp>
2018-05-21 20:24:53 +0900

    dnn-research

--- comparerate.py
+++ comparerate.py
@@ -5,19 +5,19 @@ import compare
 import train_neuralnet
 
 train_num1 = 1000
-epoch_num1 = 500
-hidden_num1 = 30
+epoch_num1 = 3000
+hidden_num1 = 1000
 batch_size1 = 100
 learning_rate1 = 0.1
 
 train_num2 = 1000
-epoch_num2 = 500
-hidden_num2 =30
+epoch_num2 = 3000
+hidden_num2 =1000
 batch_size2 = 100
 learning_rate2 = 0.1
 
 ratechange = False
-regression = True
+regression = False
 
 for i in range(10):
     train_neuralnet.train_neuralnet(train_num1,epoch_num1,hidden_num1,batch_size1,learning_rate1,data_num = 1,ratechange = ratechange,regression = regression)
