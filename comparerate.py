1acb17eb78f11ab853fdf503429ea75b5dd0833a
crukky <hiroki.kinoshita@nislab.doshisha.ac.jp>
2018-05-26 15:21:29 +0900

    dnn-research

--- comparerate.py
+++ comparerate.py
@@ -5,15 +5,15 @@ import compare
 import train_neuralnet
 
 train_num1 = 1000
-epoch_num1 = 500
-hidden_num1 = 30
-batch_size1 = 1000
+epoch_num1 = 700
+hidden_num1 = 300
+batch_size1 = 100
 learning_rate1 = 0.1
 
 train_num2 = 1000
-epoch_num2 = 500
-hidden_num2 =30
-batch_size2 = 1000
+epoch_num2 = 700
+hidden_num2 =300
+batch_size2 = 100
 learning_rate2 = 0.1
 
 ratechange = False
@@ -21,7 +21,6 @@ regression = False
 
 
 for i in range(5):
-    train_neuralnet.train_neuralnet(train_num1,epoch_num1,hidden_num1,batch_size1,learning_rate1,data_num = 1,ratechange = ratechange,regression = regression,seed = 4)
-    train_neuralnet.train_neuralnet(train_num2,epoch_num2,hidden_num2,batch_size2,learning_rate2,data_num = 2,ratechange = ratechange,regression = regression,seed = 4)
-
+    train_neuralnet.train_neuralnet(train_num1,epoch_num1,hidden_num1,batch_size1,learning_rate1,data_num = 1,ratechange = ratechange,regression = regression,seed = 5)
+    train_neuralnet.train_neuralnet(train_num2,epoch_num2,hidden_num2,batch_size2,learning_rate2,data_num = 2,ratechange = ratechange,regression = regression,seed = 5)
     compare.compare("data1.txt","data2.txt",regression = regression)
--- two_layer_net.py
+++ two_layer_net.py
@@ -11,6 +11,8 @@ class TwoLayerNet:
 
     def __init__(self, input_size, hidden_size, output_size, weight_init_std = 0.01):
         # 重みの初期化
+        weight_init_std = 0.001
+
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
